4db6306f911d977d1ad79e3db17df1daae1d2353
Tomasl77 <lobgeois.tomas@free.fr>
2019-04-08 14:58:53 +0200

    New github.io

---
+++ main.js
@@ -0,0 +1,33 @@
+var txtintro = "Press enter!";
+
+
+function intro() {
+	document.getElementById('suite').style.display = 'none'
+	display = document.getElementById('intro');
+
+	for(var i = 0, l = txtintro.length; i < l; i++) {
+	(function(i) {
+		setTimeout(function() {
+			display.innerHTML += txtintro.charAt(i);
+			}, i * 200);
+		}(i));
+	}	
+}
+
+document.querySelector('body').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    var randnum1 = Math.floor(Math.random() * 11);
+    var randnum2 = Math.floor(Math.random() * 11);
+    var fnum = randnum1;
+    var snum = randnum2;
+    var ansnum = fnum + snum;
+    if (key === 13) { 
+    	var question = prompt("Checking humanity. Please answer this : \n"+fnum+"+"+snum+" =");
+    	if(question == ansnum){
+	    	document.getElementById('intro').style.display = 'none';
+    		document.getElementById('suite').style.display = 'block';
+    		document.body.style.backgroundColor = 'white'
+    	}
+    	else alert('Verification failed. Please, try again !!');
+    }
+});
