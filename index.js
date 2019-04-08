d28a47a0739b06a927eba871cfdd57c6f2019b0c
Tomasl77 <lobgeois.tomas@free.fr>
2019-04-08 16:35:28 +0200

    test screen

--- index.js
+++ index.js
@@ -15,7 +15,24 @@ function intro() {
 }
 
 document.querySelector('body').addEventListener('keypress', function (e) {
-    var key = e.which || e.keyCode || e.touchstart;
+    var key = e.which || e.keyCode;
+    var fnum = Math.floor(Math.random() * 11);
+    var snum = Math.floor(Math.random() * 11);
+    var ansnum = fnum + snum;
+    if (key === 13) { 
+    	var question = prompt("Checking humanity. Please answer this : \n"+fnum+"+"+snum+" =");
+    	if(question == ansnum){
+	    	document.getElementById('intro').style.display = 'none';
+    		document.getElementById('suite').style.display = 'block';
+    		document.body.style.backgroundImage = "url(./images/ep_naturalwhite.png) repeat";
+    	}
+    	else alert('Verification failed. Please, try again !!');
+    }
+});
+
+document.querySelector('body').addEventListener('touchenter', function (e) {
+	
+	var key = e.which || e.keyCode;
     var fnum = Math.floor(Math.random() * 11);
     var snum = Math.floor(Math.random() * 11);
     var ansnum = fnum + snum;
