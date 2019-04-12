3bec35a8409f0bc0b4030b994487967839b2ae5c
Tomasl77 <lobgeois.tomas@free.fr>
2019-04-12 12:07:22 +0200

    js on work

--- index.js
+++ index.js
@@ -39,3 +39,96 @@ function writetitle(txt){
 function writecorps(txt) {
 	document.getElementById('corps').innerHTML = txt;
 }
+
+function html() {
+  var elem = document.getElementById("1");   
+  var width = 0;
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function css() {
+  var elem2 = document.getElementById("2");   
+  var width = 0;
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem2.style.width = width + '%'; 
+      elem2.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function js() {
+  var elem3 = document.getElementById("3");   
+  var width = 0;
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (width >= 50) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem3.style.width = width + '%'; 
+      elem3.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function php() {
+  var elem4 = document.getElementById("4");   
+  var width = 0;
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (width >= 30) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem4.style.width = width + '%'; 
+      elem4.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function sql() {
+  var elem5 = document.getElementById("5");   
+  var width = 0;
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (width >= 30) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem5.style.width = width + '%'; 
+      elem5.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function anglais() {
+  var elem6 = document.getElementById("6");   
+  var width = 0;
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (width >= 85) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem6.style.width = width + '%'; 
+      elem6.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
+function changeBg(){
+var y = document.getElementsByClassName("bar");
+var i;
+for (i = 0; i < y.length; i++) {
+  y[i].style.backgroundColor = 'rgb(55,69,77)';
+  }
+ }
